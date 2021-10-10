3d739e452ec965c8d27be43f23c5c47c3421cbaa
danodic <danodic@outlook.com>
2021-10-09 23:59:17 -0300

    Not really and improvement, but yeah,,

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,13 @@
 from cx_Freeze import setup, Executable
 import os
 
-os.environ['TCL_LIBRARY'] = "C:\\Users\\danilo\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
-os.environ['TK_LIBRARY'] = "C:\\Users\\danilo\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
+pythonpath = "T:\Programs\Python\Python310"
 
-include_files=["C:\\Users\\danilo\\AppData\\Local\\Programs\\Python\\Python36-32\\DLLs\\tcl86t.dll",
-               "C:\\Users\\danilo\\AppData\\Local\\Programs\\Python\\Python36-32\\DLLs\\tk86t.dll",
+os.environ['TCL_LIBRARY'] = os.path.join(pythonpath, "tcl\\tcl8.6")
+os.environ['TK_LIBRARY'] = os.path.join(pythonpath, "tcl\\tk8.6")
+
+include_files=[os.path.join(pythonpath, "DLLs\\tcl86t.dll"),
+               os.path.join(pythonpath, "DLLs\\tk86t.dll"),
                ".\\modes",
                ".\\keyboard",
                ".\\maps"]
@@ -16,7 +18,7 @@ executables = [Executable("main.py", base=base)]
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    'packages': ["tkinter","mido","os","yaml","threading","time"],
+    'packages': ["tkinter","mido","rtmidi", "os","yaml","threading","time"],
     'include_files': include_files,
     'excludes': ['unittest', 'test']
 }
@@ -26,7 +28,7 @@ base = "Win32GUI"
 
 setup (
     name = "pianopad",
-    version = "0.1",
+    version = "0.2",
     description = "El perro, el perro, is mi corazon...",
     options = {"build_exe": build_exe_options},
     executables = [Executable("pianopad.py", base=base)]
